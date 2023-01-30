c53f4f4d2029aae5e5b8c21e3f427016891e1b3d
Manoel Oliveira <manoelvoli@hotmail.com>
2023-01-30 20:17:40 -0300

    Criação de um novo botão voltar

---
+++ myapp/src/VoltarSelecionar.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { trocarEstadoDoJogo } from './Estado.js';
+</script>
+
+
+<div class='voltar' on:click={() => trocarEstadoDoJogo('selecionarmascote')}>
+	Voltar 
+</div>
+
+<style>
+            div.voltar {
+                width: 100%;
+                background: #7b1942;
+                text-align: center;
+                border-radius: 10px;
+                padding: 5px;
+                font-family: 'Roboto Condensed';
+                color: white;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                box-shadow: 0px 0px 12px rgb(0, 0, 0);
+            }
+</style>
