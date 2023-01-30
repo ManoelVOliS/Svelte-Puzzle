6a0656af08a25d1cb181ce0b18ef7448678bbdc3
Manoel Oliveira <manoelvoli@hotmail.com>
2023-01-30 20:17:19 -0300

    Alteração na div e mudança de cor

--- myapp/src/VoltarMenu.svelte
+++ myapp/src/VoltarMenu.svelte
@@ -4,13 +4,14 @@
 
 
 <div class='voltar' on:click={() => trocarEstadoDoJogo('menu')}>
-	Voltar ao menu
+	Voltar
 </div>
+<div></div>
 
 <style>
             div.voltar {
                 width: 100%;
-                background: #8A2A2A;
+                background: #7b1942;
                 text-align: center;
                 border-radius: 10px;
                 padding: 5px;
@@ -20,6 +21,6 @@
                 flex-direction: column;
                 align-items: center;
                 justify-content: center;
-                box-shadow: 0px 0px 12px gray;
+                box-shadow: 0px 0px 12px rgb(0, 0, 0);
             }
 </style>
