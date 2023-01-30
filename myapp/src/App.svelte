b48da2beb71be57c7053499eebc346d184d6ba23
Manoel Oliveira <manoelvoli@hotmail.com>
2023-01-30 19:53:12 -0300

    Removendo as imagens antigas

--- myapp/src/App.svelte
+++ myapp/src/App.svelte
@@ -1,27 +1,67 @@
 <script>
 
-  import Jogo from './Jogo.svelte';
+  import JogoLaeeb from './JogoLaeeb.svelte';
+  import JogoZabivaka from './JogoZabivaka.svelte';
+  import JogoFuleco from './JogoFuleco.svelte';
 	import Sobre from './Sobre.svelte';
 	import Menu from './Menu.svelte';
+  import SelecionarMascote from './SelecionarMascote.svelte';
 
   import { estado } from './Estado';
+
+  let musica = new Audio ("./musica.mp3")
+  let Pause = true
+
+  function TocarMusica(){
+        if(Pause == true){
+            musica.play()
+            Pause = false
+         
+        }else{
+            musica.pause()
+            Pause = true
+
+          }
+
+        }
+
 </script>
 
 <main>
   
   {#if $estado === 'menu'}
     <Menu/>
+  {:else if $estado === 'selecionarmascote'}
+    <SelecionarMascote/>
   {:else if $estado === 'sobre'}
     <Sobre/>
-  {:else if $estado === 'jogo'}
-    <Jogo/>
+  {:else if $estado === 'jogoLaeeb'}
+    <JogoLaeeb/>
+  {:else if $estado === 'jogoZabivaka'}
+    <JogoZabivaka/>
+  {:else if $estado === 'jogoFuleco'}
+    <JogoFuleco/>
   {/if}
+
+  <div id="Music" on:click={(TocarMusica)}></div>
 
 </main>
 
 <style>
+  
+  #Music {
+    background-image: url(../public/speakers/speaker-off.png);
+    background-size: cover;
+    cursor: pointer;
+    width: 50px;
+    height: 50px;
+    position: absolute;
+    left: 2%;
+    bottom: 2%;
+  }
+
 main {
-    background-image: url('https://media1.giphy.com/media/2jMxXMfsJVJelv062L/giphy.gif?cid=ecf05e47revf0z25zzyem1tn9yc8k655tvsdi23siy985tfi&rid=giphy.gif&ct=g');
+    background-image: url('../public/worldcup.jpg');
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center;
@@ -30,4 +70,6 @@ main {
     display: flex;
     flex-direction: column;
   }
+
+
 </style>
